9ad297612391ab225bbe9178cd8f9dee4d5d8b6b
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-10-14 21:36:08 +0100

    add sum solution

--- Arrays-Hashset/ContainsDuplicate.cs
+++ Arrays-Hashset/ContainsDuplicate.cs
@@ -101,3 +101,44 @@ public class Anagram
     }
 }
 
+public class SumTarget
+{
+    /*
+     * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+       You may assume that each input would have exactly one solution, and you may not use the same element twice.
+       You can return the answer in any order.
+
+        Example 1:
+
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+        Example 2:
+
+        Input: nums = [3,2,4], target = 6
+        Output: [1,2]
+        Example 3:
+
+        Input: nums = [3,3], target = 6
+        Output: [0,1]
+     */
+
+    public static int[] TwoSum(int target = 9)
+    {
+        int[] nums = {2, 7, 11, 15};
+
+        var values = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var diff = target - nums[i];
+
+            if (values.ContainsKey(diff))
+                return new int[] { values[diff], i };
+            else
+                values.Add(nums[i], i);
+        };
+
+        return Array.Empty<int>();
+    }
+}
