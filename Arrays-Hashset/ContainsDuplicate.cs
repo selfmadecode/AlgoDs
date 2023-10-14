b072dabd475dd6bbb420668aad98bac0b002804d
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2023-10-14 21:35:10 +0100

    Add project files.

---
+++ Arrays-Hashset/ContainsDuplicate.cs
@@ -0,0 +1,35 @@
+﻿namespace AlgoDs.Arrays_Hashset;
+
+public class ContainsDuplicate
+{
+    /*
+     * Question:
+     
+        Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+        Example 1:
+
+        Input: nums = [1,2,3,1]
+        Output: true
+        Example 2:
+
+        Input: nums = [1,2,3,4]
+        Output: false
+        Example 3:
+
+        Input: nums = [1,1,1,3,3,4,3,2,4,2]
+        Output: true
+     */
+
+    public static bool ContainsDuplicateSolution(int[] nums)
+    {
+        var noDuplicate = new HashSet<int>(); // space complexity O(n)
+        foreach (int item in nums) // time complexity O(n)
+        {
+            if (noDuplicate.Contains(item))
+                return true;
+
+            noDuplicate.Add(item);
+        }
+        return false;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlgoDs.Arrays_Hashset;
+
+
+int[] nums = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+
+//var containsDuplicate = ContainsDuplicate.ContainsDuplicateSolution(nums);
+//var isAnagram = Anagram.IsAnagram();
+//var dd = SumTarget.TwoSum();
+Console.WriteLine("Hello, World!");
+
+
